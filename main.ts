17e5bd8abb66df0c7bc6ea6c333551263f3abba7
crouse12 <crousekimo@yahoo.com.tw>
2019-09-13 10:31:03 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -138,6 +138,7 @@ namespace microbittonodemcu {
     //% blockId=thingspeak4 
     //% block="Connect to Thingspeak key %key | Write fields value | field1 value %value1 || field2 value %value2 field3 value %value3 field4 value %value4 field5 value %value5 field6 value %value6 field7 value %value7"
     //% weight=101  
+    //% blockExternalInputs=1
     export function thingspeak4(key:string, value1: number, value2?:number, value3?:number, value4?:number, value5?:number, value6?:number, value7?:number) {    
         let b=""
         let i
