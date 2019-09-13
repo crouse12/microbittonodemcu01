fe6feb7a9322412028b6a1b24b1aa173f10e0589
crouse12 <crousekimo@yahoo.com.tw>
2019-09-13 10:12:27 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -147,7 +147,7 @@ namespace microbittonodemcu {
               if (i==0)
               {
                     b=value12[0].toString()
-              }else if (value12[i].toString()!=undefined || value12[i]!=null)
+              }else if (typeof(value12[i].toString())!="undefined" || value12[i]!=null)
               {
                     let c=i+1
                     b=b+"\&field"+c.toString()+"="+value12[i].toString()
