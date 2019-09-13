792a370a2f86d6a529377863012e083f074e8560
crouse12 <crousekimo@yahoo.com.tw>
2019-09-13 09:38:07 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -137,7 +137,7 @@ namespace microbittonodemcu {
       
     //% blockId=thingspeak4 block="Connect to Thingspeak key %key | Write fields value | field1 value %value1 || field2 value %value2 field3 value %value3 field4 value %value4  field5 value %value5 field6 value %value6 field7 value %value7"
     //% weight=101  expandableArgumentMode=toggle 
-    //% inlineInputMode=inline
+    //% inlineInputMode=inline blockGap=3
     export function thingspeak4(key:string, value1: number, value2?:number, value3?:number, value4?:number, value5?:number, value6?:number, valye77?:number) {
  //       let a=value1.length
         let b=""
